cd79e4b9c9961fc14795bb2300401a81be5eeb81
sonukumarji7479 <sonukumar9303343@gmail.com>
2024-12-08 11:37:49 +0530

    Initial commit

---
+++ Assing1.py
@@ -0,0 +1,28 @@
+#this is my first program in python programing  launguage.
+
+x=5
+y="hello world"
+print("hello world");
+
+print(x)
+print(y)
+
+#this is coment 
+print("THIS IS A MY FIRST PROGRAM")
+
+print("welcome python programming") #this is comment
+
+
+# MULTILINE COMMENTS
+# This is a coment 
+# written in 
+# more than just one line
+print("This is multiline comment");
+
+#   MULTILINE CIMMMENTS
+
+"""This is a comment
+written in 
+more than just one line """
+
+print("Hello, World!")
---
+++ Assing2.py
@@ -0,0 +1,38 @@
+# PYTHON VARIABLE
+
+x=4                #x is of type int 
+x="Hello"         # x is now of type string
+print(x)
+
+# CASTING
+"""If you want to specify the data type of variable ,this
+can be done with casting """
+
+x = str(3)           # x will be '3'
+
+y = int(3)           # y will be 3
+
+z = float(3)         # z will be 3.0
+
+print(x)
+print(y)
+print(z)
+
+
+# TYPE FUNCTION 
+
+x = 5
+y = "hello sir"
+
+print(type(x))
+print(type(y))
+
+# SINGAL DOUBLE QUOTES
+
+a = 5
+b = "Hello sir"
+c = 'Hello mam'
+
+print(type(a))
+print(type(b))
+print(type(c))
